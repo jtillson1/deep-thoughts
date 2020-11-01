37b299c3c3b0b14fb07eb187a37bdc24b82565f0
jtillson1 <65985044+jtillson1@users.noreply.github.com>
2020-11-01 15:29:32 -0600

    reactions rendering

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -30,16 +30,25 @@ const ThoughtForm = () => {
     };
     const [addThought, { error }] = useMutation(ADD_THOUGHT, {
         update(cache, { data: { addThought } }) {
-          // read what's currently in the cache
-          const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
-      
-          // prepend the newest thought to the front of the array
-          cache.writeQuery({
-            query: QUERY_THOUGHTS,
-            data: { thoughts: [addThought, ...thoughts] }
-          });
+            try {
+                // could potentially not exist yet, so wrap in a try...catch
+                const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
+                cache.writeQuery({
+                    query: QUERY_THOUGHTS,
+                    data: { thoughts: [addThought, ...thoughts] }
+                });
+            } catch (e) {
+                console.error(e);
+            }
+
+            // update me object's cache, appending new thought to the end of the array
+            const { me } = cache.readQuery({ query: QUERY_ME });
+            cache.writeQuery({
+                query: QUERY_ME,
+                data: { me: { ...me, thoughts: [...me.thoughts, addThought] } }
+            });
         }
-      });
+    });
     return (
         <div>
             <p className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}>
