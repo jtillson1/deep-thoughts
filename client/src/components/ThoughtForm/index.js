e616c443906c341c95cc5deaae0abaf436ebde39
jtillson1 <65985044+jtillson1@users.noreply.github.com>
2020-11-01 15:09:00 -0600

    addThought rendering new thought to the top of the array" "

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
+import { ADD_THOUGHT } from '../../utils/mutations';
+import { useMutation } from '@apollo/react-hooks';
+import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
 
 const ThoughtForm = () => {
     const [thoughtText, setText] = useState('');
     const [characterCount, setCharacterCount] = useState(0);
+
     const handleChange = event => {
         if (event.target.value.length <= 280) {
             setText(event.target.value);
@@ -11,16 +15,39 @@ const ThoughtForm = () => {
     };
     const handleFormSubmit = async event => {
         event.preventDefault();
-        setText('');
-        setCharacterCount(0)
+
+        try {
+            // add thought to database
+            await addThought({
+                variables: { thoughtText }
+            });
+
+            setText('');
+            setCharacterCount(0);
+        } catch (e) {
+            console.error(e);
+        }
     };
+    const [addThought, { error }] = useMutation(ADD_THOUGHT, {
+        update(cache, { data: { addThought } }) {
+          // read what's currently in the cache
+          const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
+      
+          // prepend the newest thought to the front of the array
+          cache.writeQuery({
+            query: QUERY_THOUGHTS,
+            data: { thoughts: [addThought, ...thoughts] }
+          });
+        }
+      });
     return (
         <div>
-            <p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
+            <p className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}>
                 Character Count: {characterCount}/280
+                {error && <span className="ml-2">Something went wrong...</span>}
             </p>
             <form className="flex-row justify-center justify-space-between-md align-stretch"
-            onSubmit={handleFormSubmit}>
+                onSubmit={handleFormSubmit}>
                 <textarea
                     placeholder="Here's a new thought..."
                     value={thoughtText}
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -35,3 +35,17 @@ export const ADD_FRIEND = gql`
     }
   }
 `;
+export const ADD_THOUGHT = gql`
+  mutation addThought($thoughtText: String!) {
+    addThought(thoughtText: $thoughtText) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+      }
+    }
+  }
+`;
